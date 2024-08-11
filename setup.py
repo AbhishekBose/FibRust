665a1b817d8324582a8fa8f18991831eebf4574e
Abhishek Bose <abose550@gmail.com>
2024-08-11 14:06:38 +0530

    added fib module

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import find_packages, setup
    
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
 setup(
     name="FibRust",
     version="0.0.1",
     author="Abhishek Bose",
     author_email="abose@gmail.com",
     description="Calculates a Fibonacci number",
-    long_description="A basic library that calculates Fibonacci numbers",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/AbhishekBose/FibRust,
+    url="https://github.com/AbhishekBose/FibRust",
     install_requires=[],
     packages=find_packages(exclude=("tests",)),
     classifiers=[
